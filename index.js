bb3d6acb0cdbe4ad5133c93f9b585ac2f1ddd3df
M-jithendra-suryavarma <suriyamp7550@gmail.com>
2021-11-27 17:28:26 +0530

    make it better

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const path = require('path')
 require('express-async-errors');
 require('dotenv').config();
+
 const express = require('express');
 const app = express()
 //// security
 const cors = require('cors')
+const ratelimit = require('express-rate-limit')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+
+
+app.use(helmet())
+app.use(xss())
+app.use(ratelimit())
+
 app.use(cors())
 app.options(cors())
 /// default route
 app.get('/',(req,res) => res.json({msg: "hello world from backend"}))
 
-/// static file
-app.use(express.static(path.resolve(__dirname,'../client/build')))
+const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+
 
 
 //// middleware   import
@@ -37,7 +47,8 @@ app.use('/api/v1/auth',userRoute);
 app.use('/api/v1/exercise',authenticationMiddleware,exerciseRoute);  
 
 
-const port = process.env.PORT || 5000
+
+
 
 
 // use middleware
